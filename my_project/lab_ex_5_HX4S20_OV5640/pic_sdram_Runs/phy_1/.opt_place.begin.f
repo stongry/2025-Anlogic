7c87a550ca456d95e38597f759630b3819629d63
Horizon <asd219799796@qq.com>
2025-09-25 21:14:28 +0800

    整合了摄像头和udp
    已验证

--- my_project/lab_ex_5_HX4S20_OV5640/pic_sdram_Runs/phy_1/.opt_place.begin.f
+++ my_project/lab_ex_5_HX4S20_OV5640/pic_sdram_Runs/phy_1/.opt_place.begin.f
@@ -1,5 +1,5 @@
 <?xml version="1.0"?>
 <ProcessHandle Version="1" Minor="0">
-    <Process Ownner="Administrator" Host="WIN-61TPHHLRMOG" Pid="17848">
+    <Process Ownner="Administrator" Host="WIN-61TPHHLRMOG" Pid="32144">
     </Process>
 </ProcessHandle>
